179bf3b6ae8f9c0db2d98b988b6d34cec37c78c6
aramunn <aramunn@users.noreply.github.com>
2017-03-26 00:33:57 -0400

    testing stuff

--- QuestLogImproved.lua
+++ QuestLogImproved.lua
@@ -1,6 +1,50 @@
 local QuestLogImproved = {}
 
+function QuestLogImproved:HookApolloLoadForm()
+  local addonQuestLog = Apollo.GetAddon("QuestLog")
+  if not addonQuestLog then
+    Print("addonQuestLog is nil")
+    return
+  end
+  local funcLoadForm = Apollo.LoadForm
+  Apollo.LoadForm = function(xmlDoc, strForm, wndParent, addon, ...)
+    if addon == addonQuestLog then
+      Print("QuestLog loading "..strForm)
+    end
+    return funcLoadForm(xmlDoc, strForm, wndParent, addon, ...)
+  end
+end
 
+function QuestLogImproved:MakeQuestLogXmlModifications()
+  local addon = Apollo.GetAddon("QuestLog")
+  if not addon then
+    Print("addon is nil")
+    return
+  end
+  if not addon.xmlDoc:IsLoaded() then
+    Print("not loaded")
+    return
+  end
+  local tXml = addon.xmlDoc:ToTable()
+  for idx, tForm in pairs(tXml) do
+    if tForm.Name == "TopLevelItem" then
+      Print("was: "..tForm.Sprite)
+      -- tForm.Sprite = "BasicSprites:WhiteFill"
+      -- Print("now: "..tForm.Sprite)
+    end
+  end
+  addon.xmlDoc = XmlDoc.CreateFromTable(tXml)
+end
+
+function QuestLogImproved:MakeQuestLogModifications()
+  local addon = Apollo.GetAddon("QuestLog")
+  if not addon then return end
+  for idx, wndTop in ipairs(addon.wndLeftSideScroll:GetChildren()) do
+    Print("was: "..tostring(wndTop:GetSprite()))
+    wndTop:SetSprite("WhiteFill")
+    Print("now: "..tostring(wndTop:GetSprite()))
+  end
+end
 
 function QuestLogImproved:OnSave(eLevel)
   if eLevel ~= GameLib.CodeEnumAddonSaveLevel.Account then
@@ -26,8 +70,13 @@ end
 
 function QuestLogImproved:OnLoad()
   self.xmlDoc = XmlDoc.CreateFromFile("QuestLogImproved.xml")
-  -- self.xmlDoc:RegisterCallback("OnDocumentReady", self)
-  -- Apollo.RegisterSlashCommand("hacksbyaramunn", "LoadMainWindow", self)
+  self.xmlDoc:RegisterCallback("OnDocumentReady", self)
+  Apollo.RegisterSlashCommand("testqli", "MakeQuestLogModifications", self)
+  Apollo.RegisterSlashCommand("testqli2", "MakeQuestLogXmlModifications", self)
+  Apollo.RegisterSlashCommand("testqli3", "HookApolloLoadForm", self)
+end
+
+function QuestLogImproved:OnDocumentReady()
 end
 
 local QuestLogImprovedInst = QuestLogImproved:new()
