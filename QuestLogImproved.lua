a24fc4d3309d3744b3511c537dca6c37b1b882f8
aramunn <aramunn@users.noreply.github.com>
2017-04-09 15:53:37 -0400

    port context menu stuff

--- QuestLogImproved.lua
+++ QuestLogImproved.lua
@@ -1,5 +1,12 @@
 local QuestLogImproved = {}
 
+function QuestLogImproved:MeasureWindows()
+  local wndMeasure = Apollo.LoadForm(self.xmlDoc, "ContextMenuQuestLogForm", nil, self)
+  self.knContextMenuWidth = wndMeasure:GetWidth()
+  self.knContextMenuHeight = wndMeasure:GetHeight()
+  wndMeasure:Destroy()
+end
+
 function QuestLogImproved:HookQuestLogAddon()
   self.addonQuestLog = Apollo.GetAddon("QuestLog")
   if not self.addonQuestLog then
@@ -21,8 +28,16 @@ function QuestLogImproved:HookQuestLogAddon()
     self:SetCheckAllLeftSide(true)
   end
   
-  self.addonQuestLog.OnQuestItemMouseButtonUp = function (ref, wndHandler, wndControl)
-    self:ShowContextMenu(wndControl)
+  self.addonQuestLog.OnBotMouseButtonUp = function (ref, wndHandler, wndControl, eMouseButton)
+    if eMouseButton == GameLib.CodeEnumInputMouse.Right then self:ShowContextMenu(wndControl, 1) end
+  end
+  
+  self.addonQuestLog.OnMidMouseButtonUp = function (ref, wndHandler, wndControl, eMouseButton)
+    if eMouseButton == GameLib.CodeEnumInputMouse.Right then self:ShowContextMenu(wndControl, 2) end
+  end
+  
+  self.addonQuestLog.OnTopMouseButtonUp = function (ref, wndHandler, wndControl, eMouseButton)
+    if eMouseButton == GameLib.CodeEnumInputMouse.Right then self:ShowContextMenu(wndControl, 3) end
   end
 end
 
@@ -55,8 +70,62 @@ function QuestLogImproved:SetCheckAllLeftSide(bChecked)
   self.addonQuestLog:ResizeTree()
 end
 
-function QuestLogImproved:ShowContextMenu(wnd)
-  Print("here with: "..tostring(wnd:GetName()))
+function QuestLogImproved:ShowContextMenu(wnd, nLevel)
+  if self.wndContextMenu and self.wndContextMenu:IsValid() then
+    self.wndContextMenu:Destroy()
+  end
+  self.wndContextMenu = Apollo.LoadForm(self.xmlDoc, "ContextMenuQuestLogForm", "TooltipStratum", self)
+  local wndButtonList = self.wndContextMenu:FindChild("ButtonList")
+  self.wndContextMenu:SetData({ level = nLevel, window = wnd })
+  self.wndContextMenu:Invoke()
+  local tCursor = Apollo.GetMouse()
+  local tPos = { x = (tCursor.x - 10), y = (tCursor.y - 25) }
+  local tScreen = Apollo.GetDisplaySize()
+  if tPos.x + self.knContextMenuWidth > tScreen.nWidth then tPos.x = tPos.x - self.knContextMenuWidth + 10*2 end
+  if tPos.y + self.knContextMenuHeight > tScreen.nHeight then tPos.y = tPos.y - self.knContextMenuHeight + 25*2 end
+  self.wndContextMenu:Move(tPos.x, tPos.y, self.knContextMenuWidth, self.knContextMenuHeight)
+end
+
+function QuestLogImproved:OnRegularBtn(wndHandler, wndControl)
+  local tData = self.wndContextMenu:GetData()
+  local strButtonName = wndHandler:GetName()
+  self:HandleContextMenuButton(strButtonName, tData.window, tData.level)
+  if strButtonName == "BtnAbandon" then
+    self.wndContextMenu:FindChild("Confirm:List"):ArrangeChildrenVert(Window.CodeEnumArrangeOrigin.LeftOrTop)
+    self.wndContextMenu:FindChild("Confirm:List"):RecalculateContentExtents()
+    self.wndContextMenu:FindChild("Confirm:Header"):SetText("Abandon these quests?")
+    self.wndContextMenu:FindChild("Confirm:ConfirmBtn"):SetName("BtnAbandonConfirm")
+    self.wndContextMenu:FindChild("Confirm"):Show(true)
+  else
+    self.wndContextMenu:Destroy()
+    self.addonQuestLog:RedrawLeftTree()
+  end
+  if strButtonName == "BtnAbandonConfirm" then
+    self.addonQuestLog:DestroyAndRedraw()
+  end
+end
+
+function QuestLogImproved:HandleContextMenuButton(strButtonName, wnd, nLevel)
+  if nLevel == 1 then
+    local quest = wnd:GetData()
+    local eState = quest:GetState()
+    if strButtonName == "BtnAbandon" and quest:CanAbandon() then
+      local wndQuestTitle = Apollo.LoadForm(self.xmlDoc, "ContextMenuConfirmListItem", self.wndContextMenu:FindChild("Confirm:List"), self)
+      wndQuestTitle:FindChild("Text"):SetText(quest:GetTitle())
+    end
+    if strButtonName == "BtnAbandonConfirm" and quest:CanAbandon() then quest:Abandon() end
+    if strButtonName == "BtnIgnore" and (eState == Quest.QuestState_Abandoned or eState == Quest.QuestState_Mentioned) then quest:ToggleIgnored() end
+    if strButtonName == "BtnTrack" then quest:SetTracked(true) end
+    if strButtonName == "BtnUntrack" then quest:SetTracked(false) end
+  else
+    local tWindowNames
+    if nLevel == 2 then tWindowNames = { items = "MiddleLevelItems", button = "BottomLevelBtn" } end
+    if nLevel == 3 then tWindowNames = { items = "TopLevelItems", button = "MiddleLevelTitle" } end
+    if not tWindowNames then return end
+    for idx, wndItem in pairs(wnd:GetParent():FindChild(tWindowNames.items):GetChildren()) do
+      self:HandleContextMenuButton(strButtonName, wndItem:FindChild(tWindowNames.button), nLevel - 1)
+    end
+  end
 end
 
 function QuestLogImproved:HookApolloLoadForm()
@@ -100,6 +169,7 @@ function QuestLogImproved:OnLoad()
 end
 
 function QuestLogImproved:OnDocumentReady()
+  self:MeasureWindows()
   self:HookQuestLogAddon()
   self:HookApolloLoadForm()
 end
